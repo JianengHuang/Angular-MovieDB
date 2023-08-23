6d1ac9c0eb960a9e30275670830a5a53bb878840
Jianeng Huang <jianengh@gmail.com>
2023-08-23 04:56:50 +0200

    add display of movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MovieComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
