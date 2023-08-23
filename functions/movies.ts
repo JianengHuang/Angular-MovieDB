3598969fce44d2e7ad8bbeb090860f8f2ff95579
Jianeng Huang <jianengh@gmail.com>
2023-08-23 05:14:59 +0200

    add dotenv

--- functions/movies.ts
+++ functions/movies.ts
@@ -1,15 +1,16 @@
 import { type Handler } from '@netlify/functions';
-import { environment } from 'src/environments/environment';
+import {} from 'dotenv/config';
 
 const handler: Handler = async (event) => {
   let queryparams = '';
+  const api_key = process.env['API_KEY'];
 
   for (const key in event.queryStringParameters) {
     queryparams += `${key}=${event.queryStringParameters[key]}&`;
   }
 
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=${environment.api_key}&${queryparams}`
+    `http://www.omdbapi.com/?apikey=${api_key}&${queryparams}`
   );
 
   const json = await response.json();
