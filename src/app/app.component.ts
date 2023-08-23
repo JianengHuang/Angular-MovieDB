6a29830d15a20e2cf28382cdfab22b15ce43203e
Jianeng Huang <jianengh@gmail.com>
2023-08-23 03:33:12 +0200

    add form for movie search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, type OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-moviedb';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('Start of the project');
+  }
+
+  title: string = '';
+
+  getMovies(title: string) {
+    fetch(`/api/movies/?t=${title}`)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
 }
