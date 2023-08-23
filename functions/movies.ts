936177a5cc5a114d2386f87875908b7322023565
Jianeng Huang <jianengh@gmail.com>
2023-08-23 04:53:48 +0200

    update api route to add all query parameters

--- functions/movies.ts
+++ functions/movies.ts
@@ -2,10 +2,14 @@ import { type Handler } from '@netlify/functions';
 import { environment } from 'src/environments/environment';
 
 const handler: Handler = async (event) => {
-  const s = event.queryStringParameters?.['s'] ?? 'batman';
+  let queryparams = '';
+
+  for (const key in event.queryStringParameters) {
+    queryparams += `${key}=${event.queryStringParameters[key]}&`;
+  }
 
   const response = await fetch(
-    `http://www.omdbapi.com/?apikey=${environment.api_key}&s=${s}`
+    `http://www.omdbapi.com/?apikey=${environment.api_key}&${queryparams}`
   );
 
   const json = await response.json();
